ab1d0c362d788649629c7b3d216d32c01d8535a5
zaidu <zaid@gmail.com>
2024-12-11 13:38:19 +0530

    vmhvhnvh

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "ThirdResourceGroup" {
   name     = "rg3"
   location = "East Europe"
 }
+
+resource "azurerm_resource_group" "FourthResourceGroup" {
+  name     = "rg4"
+  location = "East Europe"
+}
